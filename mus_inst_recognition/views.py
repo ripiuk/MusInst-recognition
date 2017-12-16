4fdc91f8ef515e2499bc732e0a43d8ad5ee3e1f4
Sany <ripiuk@ua.fm>
2017-12-16 18:16:13 +0200

    Recognize image from url

--- mus_inst_recognition/views.py
+++ mus_inst_recognition/views.py
@@ -1,12 +1,13 @@
 import os
 import shutil
 import uuid
-import asyncio
+import aiohttp
 
 from aiohttp import web
 import aiohttp_jinja2
 
 from recognition_tool.label_image import recognize_image
+
 
 def redirect(request, router_name):
     url = request.app.router[router_name].url()
@@ -47,8 +48,6 @@ class FromLocal(web.View):
             path_to_image=f'{dir_name}/{sub_dir}/' + filename
         )
         # TODO: delete files from uploaded_data
-        # shutil.rmtree(path_to_image_dir)
-        print(recognized)
         return {'eval_time': eval_time, 'recognized': recognized,
                 'image_path': f'{sub_dir}/{filename}'}
 
@@ -58,3 +57,34 @@ class ByLink(web.View):
     @aiohttp_jinja2.template('by_link.html')
     async def get(self):
         pass
+
+    @aiohttp_jinja2.template('by_link.html')
+    async def post(self):
+        data = await self.request.post()
+        url = data.get('image_url')
+        chunk_size = 100
+        filename = os.path.basename(url)
+        dir_name = 'uploaded_data'  # TODO: make it global
+        sub_dir = str(uuid.uuid4())
+
+        if filename == '':
+            return {'not_choose_file': True}
+
+        path_to_image_dir = os.path.join(os.getcwd() + f'/{dir_name}/{sub_dir}/')
+        os.mkdir(path_to_image_dir)
+        full_path_to_image = os.path.join(os.getcwd() + f'/{dir_name}/{sub_dir}/', filename)
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                with open(full_path_to_image, 'wb') as fd:
+                    while True:
+                        chunk = await resp.content.read(chunk_size)
+                        if not chunk:
+                            break
+                        fd.write(chunk)
+
+        eval_time, recognized = recognize_image(
+            path_to_image=f'{dir_name}/{sub_dir}/' + filename
+        )
+        return {'eval_time': eval_time, 'recognized': recognized,
+                'image_path': f'{sub_dir}/{filename}'}
